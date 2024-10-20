d8dddab8a517974cc11f36a25f7a1dd16f7f2f53
Arbaaz chouhan <chouhanarbaaz4@gmail.com>
2024-10-20 16:46:01 +0530

    Javascript-Projects

--- 6- Random Color Generator/preatis-file/index.js
+++
@@ -1,8 +0,0 @@
-const a = document.querySelector(".container");
-for(let i = 0; i <30; i++){
-    const b = document.createElement("div");
-    b.classList.add("color");
-    a.appendChild(b);
-}
-console.log(a);
-const b = document.querySelector(".color")
